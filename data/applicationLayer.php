c2b1eddc9502b09f8e41372b33b7ffc17acb9233
ABELARDO\Abelardo <abelardo_gonzalezg@hotmail.com>
2017-04-19 02:52:52 -0500

    Addition of files, for 3tier model, And adjustments for Login and Registration methods with new schema

---
+++ data/applicationLayer.php
@@ -0,0 +1,328 @@
+<?php
+
+header('Content-type: application/json');
+require_once __DIR__ . '/dataLayer.php';
+
+$action = $_POST["action"];
+
+switch($action){
+	case "LOGIN" : loginFunction();
+					break;
+	case "REGISTER" : RegistrationFunction();
+					break;
+	case "LOADHOME" : HomeService();
+					break;
+	case "LOADPROFILE" : ProfileSerivce();
+					break;
+	case "LOGOUT" : LogoutService();
+					break;
+	case "COMMENTING" : PostComment();
+					break;
+	case "SEARCHFRIEND" : SearchFriend();
+					break;
+	case "SENDFRIENDREQUEST" : SendFriendRequest();
+					break;
+	case "LOADFRIENDREQUESTS" : LoadFriendRequests();
+					break;
+	case "ACCEPTFRIENDREQUEST" : AcceptFriendRequest();
+					break;
+	case "REJECTFRIENDREQUEST" : RejectFriendRequest();
+					break;
+	case "LOADFRIENDLIST" : LoadFriendList();
+					break;
+
+}
+
+function loginFunction(){
+	$userName = $_POST["username"];
+	$userPassword = $_POST["userPassword"];
+	$remember = $_POST["rememberMe"];
+
+	$result = attemptLogin($userName,$remember);
+ //echo "Haleloo";
+	if ($result["status"] == "SUCCESS"){
+		$Password = decryptPassword($result["password"]);
+  //echo $Password;
+		if ($userPassword === $Password) {
+			echo json_encode(array("message" => "Login Successful"));
+		}
+
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}	
+}
+
+
+
+function RegistrationFunction(){
+	$FName = $_POST['firstName'];
+	$LName = $_POST['lastName'];
+	$userName = $_POST['username'];
+	$Email = $_POST['email'];
+	$Address = $_POST['address'];
+
+	$userPassword = encryptPassword();
+
+	$result = attemptRegistration($FName,$LName,$userName,$Email,$userPassword,$Address);
+
+	if ($result["status"] == "SUCCESS"){
+		echo json_encode(array("message" => "Registration Successful"));
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+
+}
+
+function HomeService(){
+	$username = $_POST['username'];
+
+	$result = attemptHomeService($username);
+
+	//$comentario = $result["comments"];
+
+
+	if ($result["status"] == "SUCCESS"){
+		echo json_encode($result["comments"]);
+		//echo json_encode(array($comentario));
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+
+}
+
+function ProfileSerivce(){
+	session_start();
+	
+	$username = $_SESSION["user"];
+
+	$result = attemptProfileService($username);
+
+	//echo $results["profile"]; 
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode($result["profile"]);
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+
+
+}
+
+function LogoutService(){
+	$result = attemptLogout();
+
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode(array("message" => "Logout Successful"));
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+}
+
+
+function PostComment(){
+	session_start();
+	if (isset($_SESSION["Activity"]) && (time() - $_SESSION["Activity"] < 1800)){
+		$userfName = $_SESSION["fName"] ;
+		$userlName = $_SESSION["lName"] ;
+		$username = $_SESSION["user"] ;
+		$useremail = $_SESSION["email"] ;
+	}
+	else{
+		echo json_encode(array("message" => "Session timeout"));
+	}
+
+	$comment = $_POST["comment"];
+	//echo $userfName.$userlName.$username.$useremail.$comment;
+	$result = attemptPostComment($username,$userfName,$userlName,$useremail,$comment);
+	//echo $result["status"];
+	//echo $result["comment"];
+
+	if( $result["status"] == "SUCCESS"){
+		//echo $result;
+		echo json_encode($result);
+	}
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+}
+
+function SearchFriend(){
+	
+	
+	$friend2find = $_POST["friend"];
+
+
+	$result = attemptFriendSearch($friend2find);
+
+
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode($result["friend"]);
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+}
+
+function SendFriendRequest(){
+
+	session_start();
+	
+	$username = $_SESSION["user"];
+	$newfriend = $_POST["newfriend"];
+
+	$result = attemptNewFriendRequest($username,$newfriend);
+
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode(array("message" => "Request Sent"));
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+
+
+}
+
+function LoadFriendRequests(){
+
+	session_start();
+	
+	$username = $_SESSION["user"];
+	$result = attemptLoadFriendRequests($username);
+
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode($result["friend"]);
+	}
+	elseif ($result["status"] == "REQUEST NOT FOUND") {
+			echo json_encode(array("message" => "No Request Found"));
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+}
+
+function AcceptFriendRequest(){
+	session_start();
+	
+	$username = $_SESSION["user"];
+	$newfriend = $_POST["newfriend"];
+
+	$result = attmptAcceptFriendRequest($username,$newfriend);
+
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode(array("message" => "Request ACCEPTED"));
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+
+
+}
+
+function RejectFriendRequest(){
+	session_start();
+	
+	$username = $_SESSION["user"];
+	$newfriend = $_POST["newfriend"];
+
+	$result = attmptRejectFriendRequest($username,$newfriend);
+
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode(array("message" => "Request REJECTED"));
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+}
+
+function LoadFriendList(){
+	session_start();
+	
+	$username = $_SESSION["user"];
+
+	$result = attemptLoadFriendList($username);
+
+	if ($result["status"] == "SUCCESS"){
+
+		echo json_encode($result["friendships"]);
+	}	
+	else{
+		header('HTTP/1.1 500' . $result["status"]);
+		die($result["status"]);
+	}
+
+}
+
+# Action to encrypt the password of the user
+function encryptPassword()
+{
+	$userPassword = $_POST['userPassword'];
+
+    $key = pack('H*', "bcb04b7e103a05afe34763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+    $key_size =  strlen($key);
+    
+    $plaintext = $userPassword;
+
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+    
+    $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $plaintext, MCRYPT_MODE_CBC, $iv);
+    $ciphertext = $iv . $ciphertext;
+    
+    $userPassword = base64_encode($ciphertext);
+
+    return $userPassword;
+}
+
+#Action to decrypt the password of the user
+function decryptPassword($password)
+{
+	$key = pack('H*', "bcb04b7e103a05afe34763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+    
+    $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+	
+    $ciphertext_dec = base64_decode($password);
+    $iv_dec = substr($ciphertext_dec, 0, $iv_size);
+    $ciphertext_dec = substr($ciphertext_dec, $iv_size);
+
+    $password = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+   	
+   	
+   	$count = 0;
+   	$length = strlen($password);
+
+    for ($i = $length - 1; $i >= 0; $i --)
+    {
+    	if (ord($password{$i}) === 0)
+    	{
+    		$count ++;
+    	}
+    }
+
+    $password = substr($password, 0,  $length - $count); 
+
+    return $password;
+}
+
+?>
---
+++ data/dataLayer.php
@@ -0,0 +1,516 @@
+<?php
+	
+	function connectionToDataBase(){
+		$servername = "localhost";
+		$username = "root";
+		$password = "root";
+		$dbname = "a01195884";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		
+		if ($conn->connect_error){
+			return null;
+		}
+		else{
+			return $conn;
+		}
+	}
+
+	function attemptLogin($userName,$remember){
+
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$sql = "SELECT * FROM Users WHERE username='$userName'";
+		
+			$result = $conn->query($sql);
+
+			if ($result->num_rows > 0)
+			{
+				$row = $result->fetch_assoc();
+				$conn -> close();
+				session_start();
+				$_SESSION["fName"] = $row["fName"];
+				$_SESSION["lName"] = $row["lName"];
+				$_SESSION["user"]  = $row["username"];
+				$_SESSION["email"] = $row["email"];
+
+				if($remember){
+					setcookie("user",$userName,time()+3600*24*30,"/","",0);
+				}
+
+				$_SESSION["Activity"] = time();
+				//return array("status" => "SUCCESS","passwrd" => );
+				return array("status" => "SUCCESS", "fName" => $row['fName'], "lName" => $row['lName'], "password" => $row['passwrd']);
+
+
+			}
+			else{
+				$conn -> close();
+				return array("status" => "USERNAME NOT FOUND");
+			}
+		}else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+
+	function attemptRegistration($FName,$LName,$userName,$Email,$userPassword,$Address){
+
+		$conn = connectionToDataBase();
+		if($conn != null){
+			$sqlVerif = "SELECT * FROM Users WHERE username='$userName' ";
+			$sqlInsert = "INSERT INTO Users(fName, lName, username, passwrd, email, address)
+					VALUES  ('$FName', '$LName', '$userName', '$userPassword','$Email','$Address')";
+			//echo $sqlInsert;
+			//echo $sqlVerif;
+
+			$res = $conn->query($sqlVerif);
+			
+			//if ($res->num_rows > 0)
+			//if(mysqli_query($conn,$sqlVerif))
+			if ($res->num_rows > 0)
+			{
+				$conn->close();
+				return array("status"=>"Username already taken.");
+				 //Pre-Prepares a json file with mssg
+				//header('HTTP/1.1 409 Conflict, Username already in use please select another one');
+			    //die("Username already in use.");
+	        	//echo json_encode($res);
+			}
+			else{
+				//mysqli_query($conn,$sql) //True if register inserted succesfully
+				if (mysqli_query($conn,$sqlInsert)){
+					$conn->close();
+					session_start();
+					$_SESSION["fName"] = $FName;
+					$_SESSION["lName"] = $LName;
+					$_SESSION["user"]  = $username;
+					$_SESSION["email"] = $Email;
+
+					if($remember){
+						setcookie("user",$userName,time()+3600*24*30,"/","",0);
+					}
+
+					$_SESSION["Activity"] = time();
+
+					return array("status"=>"SUCCESS");
+
+					//echo json_encode("New record created successfully");
+				}
+				else 
+				{
+					$conn->close();
+					return array("status"=>"Something went wrong on the server.");
+					//header('HTTP/1.1 500 Bad connection, something went wrong while saving your data, please try again later');
+				    //die("Error: " . $sql . "\n" . mysqli_error($conn));
+				}
+			}
+		}
+		else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+	function attemptHomeService ($username){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			
+			//$userPassword = $_POST['userPassword'];
+			
+			$conn ->set_charset('utf8mb4');
+			$sql = "SELECT username, fName, lName, email, comment FROM Users U JOIN Comments C JOIN Friends F
+					WHERE U.$username = '$userName' AND U.$username = F.$username AND F.$username = C.$username";
+
+			$sql = " SELECT * FROM Comments ";
+			$result = $conn->query($sql); 
+
+			//echo $result->num_rows;
+			if ($result->num_rows > 0)//Double check
+			{
+				$comments = array();
+				// output data of each row
+			    while($row = $result->fetch_assoc()) 
+			    {
+			    	$response = array('fName' => $row['fname'], 'lName' => $row['lname'], 'username' => $row['username'], 'comment' => $row['comment'], 'email' => $row['email']);   
+			    	array_push($comments, $response);
+			    	//echo ($response);
+				}
+
+			    //echo json_encode($response);
+			    //echo json_encode($comments);
+			    $conn->close();
+			    return array("status" =>  "SUCCESS", "comments" => $comments);
+			}
+
+			}else{
+				$conn -> close();
+				return array("status" => "CONNECTION WITH DB WENT WRONG");
+			}	
+	}
+
+
+	function attemptProfileService($username){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			$conn ->set_charset('utf8mb4');
+			//$userName = $_POST['username'];
+			//$userPassword = $_POST['userPassword'];
+			//PROFILE EXAMPLE 
+			$sql = " SELECT fName, lName, username,gender,email,country FROM Users WHERE username = '$username' ";
+			$result = $conn->query($sql); 
+
+			//echo $result->num_rows;
+			if ($result->num_rows > 0)//Double check
+			{
+				
+				// output data of each row
+			    while($row = $result->fetch_assoc()) 
+			    {
+			    	$response = array('fName' => $row['fName'],
+		    	 					'lName' => $row['lName'],
+		    	 					'username' => $row['username'], 
+		    	 					'gender' => $row['gender'], 
+		    	 					'email' => $row['email'], 
+		    	 					'country' => $row['country'] );   
+			    	//array_push($comments, $response);
+			    	
+
+				}
+				//echo ($response['fName']);
+			    //echo json_encode($response);
+			    $conn-> close();
+			    return array("status" => "SUCCESS","profile" => $response);
+
+			    //echo json_encode($result->fetch_assoc());
+			}
+			else
+			{
+				$conn -> close();
+				return array("status" => "USERNAME NOT FOUND");
+		    	//header('HTTP/1.1 406 User not found'); //Pre-Prepares a json file with mssg
+		        //die("Wrong credentials provided!"); 
+			}
+
+
+
+		}else{
+				$conn -> close();
+				return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+
+
+
+
+
+	}
+
+
+	function attemptLogout(){
+		unset($_SESSION["fName"]);
+		unset($_SESSION["lName"]);
+		unset($_SESSION["user"]);
+		unset($_SESSION["email"]);
+		unset($_SESSION["Activity"]);
+		session_destroy();
+		return array("status" => "SUCCESS");
+	}
+
+
+
+	function attemptPostComment($username,$fName,$lName,$email,$comment){
+		$conn = connectionToDataBase();
+
+		if($conn != null){
+			$sqlInsert = "INSERT INTO Comments(username, comment, fname, lname, email)
+		VALUES  ('$username', '$comment', '$fName','$lName', '$email')";
+			
+			//mysqli_query($conn,$sql) //True if register inserted succesfully
+		//$conn ->set_charset('utf8mb4');
+			if (mysqli_query($conn,$sqlInsert)){
+				$comm = "<li> 
+							$fName $lName <br/>
+							<b>User Name: </b> $username <br/>
+							Email: $email <br/>
+							Comment: $comment
+							  </li> ";
+				//echo HolaDL.$comm;
+				$conn->close();
+				return array("status"=>"SUCCESS", "comment" => $comm);
+
+				//echo json_encode("New record created successfully");
+			}
+			else 
+			{
+				$conn->close();
+				return array("status"=>"Something went wrong on the server.");
+				//header('HTTP/1.1 500 Bad connection, something went wrong while saving your data, please try again later');
+			    //die("Error: " . $sql . "\n" . mysqli_error($conn));
+			}
+			
+		}
+		else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+	function attemptFriendSearch($friend2find){
+		$conn = connectionToDataBase();
+
+		if($conn != null){
+			$conn ->set_charset('utf8mb4');
+			//$userName = $_POST['username'];
+			//$userPassword = $_POST['userPassword'];
+			//PROFILE EXAMPLE 
+			$sqlUsername = " SELECT fName, lName, username,gender,email,country FROM Users WHERE username = '$friend2find' ";
+			$sqlEmail = " SELECT fName, lName, username,gender,email,country FROM Users WHERE email = '$friend2find' ";
+			$result = $conn->query($sqlUsername); 
+			$result2 = $conn->query($sqlEmail); 
+
+			//echo $result->num_rows;
+			if ($result->num_rows > 0 || $result2->num_rows > 0)//Double check
+			{
+				if ($result->num_rows > 0){
+					// output data of each row
+				    while($row = $result->fetch_assoc()) 
+				    {
+				    	$response = array('fName' => $row['fName'],
+			    	 					'lName' => $row['lName'],
+			    	 					'username' => $row['username'], 
+			    	 					'gender' => $row['gender'], 
+			    	 					'email' => $row['email'], 
+			    	 					'country' => $row['country'] );   
+				    	//array_push($comments, $response);
+					}
+				}else{
+					while($row = $result2->fetch_assoc()) 
+				    {
+				    	$response = array('fName' => $row['fName'],
+			    	 					'lName' => $row['lName'],
+			    	 					'username' => $row['username'], 
+			    	 					'gender' => $row['gender'], 
+			    	 					'email' => $row['email'], 
+			    	 					'country' => $row['country'] );   
+				    	//array_push($comments, $response);
+					}
+				}
+			    $conn-> close();
+			    return array("status" => "SUCCESS","friend" => $response);
+
+			    //echo json_encode($result->fetch_assoc());
+			}
+			else
+			{
+				$conn -> close();
+				return array("status" => "USERNAME or EMAIL NOT FOUND");
+			}
+
+		}
+		else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+
+	function attemptNewFriendRequest($username,$newfriend){
+		$conn = connectionToDataBase();
+
+		if($conn != null){
+			$sqlInsert = "INSERT INTO Friends(username, friend,request, friendship)
+					VALUES  ('$username', '$newfriend',FALSE,TRUE),
+							('$newfriend', '$username',TRUE,FALSE)";
+	
+			if (mysqli_query($conn,$sqlInsert)){
+				
+				//echo HolaDL.$comm;
+				$conn->close();
+				return array("status"=>"SUCCESS");
+
+				//echo json_encode("New record created successfully");
+			}
+			else 
+			{
+				$conn->close();
+				return array("status"=>"Something went wrong on the server.");
+				//header('HTTP/1.1 500 Bad connection, something went wrong while saving your data, please try again later');
+			    //die("Error: " . $sql . "\n" . mysqli_error($conn));
+			}
+
+		}
+		else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+	function attemptLoadFriendRequests($username){
+		$conn = connectionToDataBase();
+
+		if($conn != null){
+			$conn ->set_charset('utf8mb4');
+			//$userName = $_POST['username'];
+			//$userPassword = $_POST['userPassword'];
+			//PROFILE EXAMPLE 
+			$sqlUsername = " SELECT username,friend FROM Friends WHERE username = '$username' AND request = TRUE ";
+			
+			$result = $conn->query($sqlUsername); 
+
+			//echo $result->num_rows;
+			if ($result->num_rows > 0)//Double check
+			{	
+				$row = $result->fetch_assoc();
+				$friend2find = $row['friend'];
+				$sqlFriend = " SELECT fName, lName, username,gender,email,country FROM Users WHERE username = '$friend2find' ";
+				$result = $conn->query($sqlFriend); 
+				// output data of each row
+			    while($row = $result->fetch_assoc()) 
+			    {
+			    	$response = array('fName' => $row['fName'],
+		    	 					'lName' => $row['lName'],
+		    	 					'username' => $row['username'], 
+		    	 					'gender' => $row['gender'], 
+		    	 					'email' => $row['email'], 
+		    	 					'country' => $row['country'] );   
+			    	//array_push($comments, $response);
+				}
+				
+
+			    $conn-> close();
+			    return array("status" => "SUCCESS","friend" => $response);
+
+			    //echo json_encode($result->fetch_assoc());
+			}
+			else
+			{
+				$conn -> close();
+				return array("status" => "REQUEST NOT FOUND");
+			}
+
+		}
+		else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+
+	function attmptAcceptFriendRequest($username,$newfriend){
+		$conn = connectionToDataBase();
+
+		if($conn != null){
+			/*$sqlInsert = "INSERT INTO Friends(username, friend,request, friendship)
+					VALUES  ('$username', '$newfriend',FALSE,TRUE),
+							('$newfriend', '$username',TRUE,FALSE)";
+			*/
+			$sqlAcceptRequest = "UPDATE Friends SET friendship = TRUE 
+						WHERE username = '$username' AND friend = '$newfriend'";
+			$sqlUpdateRequest = "UPDATE Friends SET request = FALSE 
+						WHERE username = '$username' AND friend = '$newfriend'";
+	
+			if (mysqli_query($conn,$sqlAcceptRequest) && mysqli_query($conn,$sqlUpdateRequest)){
+				
+				//echo HolaDL.$comm;
+				$conn->close();
+				return array("status"=>"SUCCESS");
+
+				//echo json_encode("New record created successfully");
+			}
+			else 
+			{
+				$conn->close();
+				return array("status"=>"Something went wrong on the server.");
+				//header('HTTP/1.1 500 Bad connection, something went wrong while saving your data, please try again later');
+			    //die("Error: " . $sql . "\n" . mysqli_error($conn));
+			}
+
+		}
+		else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+
+	function attmptRejectFriendRequest($username,$newfriend){
+		$conn = connectionToDataBase();
+
+		if($conn != null){
+			/*$sqlInsert = "INSERT INTO Friends(username, friend,request, friendship)
+					VALUES  ('$username', '$newfriend',FALSE,TRUE),
+							('$newfriend', '$username',TRUE,FALSE)";
+			*/
+			$sqlRejectRequest = "DELETE FROM Friends 
+						WHERE username = '$username' AND friend = '$newfriend' " ;
+			$sqlDeleteRequest = "DELETE FROM Friends 
+						WHERE username = '$newfriend' AND friend = '$username' " ;
+	
+			if (mysqli_query($conn,$sqlRejectRequest) && mysqli_query($conn,$sqlDeleteRequest)){
+				
+				//echo HolaDL.$comm;
+				$conn->close();
+				return array("status"=>"SUCCESS");
+
+				//echo json_encode("New record created successfully");
+			}
+			else 
+			{
+				$conn->close();
+				return array("status"=>"Something went wrong on the server.");
+				//header('HTTP/1.1 500 Bad connection, something went wrong while saving your data, please try again later');
+			    //die("Error: " . $sql . "\n" . mysqli_error($conn));
+			}
+
+		}
+		else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+
+	function attemptLoadFriendList($username){
+		$conn = connectionToDataBase();
+
+		if ($conn != null){
+			
+			//$userPassword = $_POST['userPassword'];
+			
+			$conn ->set_charset('utf8mb4');
+			$sql = "SELECT username, fName, lName, email, comment FROM Users U JOIN Comments C JOIN Friends F
+					WHERE U.$username = '$userName' AND U.$username = F.$username AND F.$username = C.$username";
+
+			$sql = " SELECT * FROM Friends WHERE username = '$username' ";
+			//echo $sql;
+			$result = $conn->query($sql); 
+
+			//echo $result->num_rows;
+			if ($result->num_rows > 0)//Double check
+			{
+				$friends = array();
+				// output data of each row
+			    while($row = $result->fetch_assoc()) 
+			    {
+			    	//$response = array('fName' => $row['fname'], 'lName' => $row['lname'], 'username' => $row['username'], 'comment' => $row['comment'], 'email' => $row['email']);  
+			    	$response = array('userFriend' => $row['friend']);
+			    	array_push($friends, $response);
+			    	//echo ($response);
+				}
+
+			    //echo json_encode($response);
+			    //echo json_encode($comments);
+			    $conn->close();
+			    return array("status" =>  "SUCCESS", "friendships" => $friends);
+			}
+		}else{
+			$conn -> close();
+			return array("status" => "CONNECTION WITH DB WENT WRONG");
+		}
+	}
+
+?>
